90f50d176c46a36848d24daeae71b5eb905cd54e
Jaswath <146360441+xboybx@users.noreply.github.com>
2025-08-07 12:09:55 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173 || https://xboy-profile-mandb-server.onrender.com",
+  origin: "https://xboy-profile-mandb-client.onrender.com || http://localhost:5173",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
